8761e6508827aa94c620641c24cfbcdaead1ccd0
94ju <janithherath94@gmail.com>
2019-09-20 19:42:02 +0530

    register form validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
   Roles: any = ['Admin', 'Developer'];
   constructor() { }
-
+  onRegister(form:NgForm){
+    console.log(form.value)
+  }
   ngOnInit() {
   }
 
