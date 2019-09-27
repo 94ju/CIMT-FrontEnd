a5b6c250064a120bfa6e0980ee9caaf73e68b92d
94ju <janithherath94@gmail.com>
2019-09-28 02:03:08 +0530

    trial table

--- src/app/components/tables/priceTable.service.ts
+++ src/app/components/tables/priceTable.service.ts
@@ -7,16 +7,31 @@ import { HttpHeaders } from '@angular/common/http';
 export class PriceTableService{
    
     private priceurl = "http://localhost:3000/api/vmdetails/"
-    
-    
+    datasource;
+    displayedColumns: string[] = ['Catergory', 'Machine Type', 'CPUs', 'Memory','Network','On-Demand-Price(Linux)','AVG.Spot Price'];
     constructor(private http:HttpClient){}
    
     getPrices(){
-            this.http.get(this.priceurl).subscribe(result=>{
-            console.log(result);
+            this.http.get<{products:string}>(this.priceurl).
+            // pipe(
+            //     map(data=>
+                   
+            //     )
+            // ).
+            subscribe(result=>{
+            this.datasource=result.products[0];
+            console.log(this.datasource);
+            console.log(this.datasource.category);
+            console.log(this.datasource.type);
+            console.log(this.datasource.onDemandPrice);
             return(result);
         })
      }
     ngOnInit() {
+        // this.http.get(this.priceurl).subscribe(result=>{
+        //     this.datasource=result;
+        //     console.log(result);
+        //     return(result);
+        // })
     }
 }
